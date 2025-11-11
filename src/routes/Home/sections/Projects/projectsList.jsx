82bb01f771f48242864d1773c3ac937414b06c7d
RaonyDev <raonysaccon@gmail.com>
2025-11-11 17:16:43 -0300

    emergency commit

--- src/routes/Home/sections/Projects/projectsList.jsx
+++ src/routes/Home/sections/Projects/projectsList.jsx
@@ -1,12 +1,5 @@
 export const projectsList = [
   {
-    title: "Box Zero 3",
-    description:
-      "Desenvolvi esse site para a oficina mecânica Box Zero 3, premiada há 8 anos como a melhor de Arroio do Sal - RS.",
-    image: "/projects/boxzero3.png",
-    url: "https://boxzero3.netlify.app",
-  },
-  {
     title: "JokerShopp",
     description:
       "Fiz essa loja virtual para uma loja de roupas estilo streetwear da cidade de Arroio do Sal - RS",
@@ -14,10 +7,24 @@ export const projectsList = [
     url: "https://jokershopp.netlify.app",
   },
   {
+    title: "Krusty Personal Car",
+    description:
+      "Desenvolvi esse site para a Krusty Personal Car, uma oficina de chapeação automotiva.",
+    image: "/projects/krusty.png",
+    url: "https://krustycar.netlify.app",
+  },
+  {
     title: "Mãe Nina D'Oxum",
     description:
       "Site da Mãe Nina de Oxum, um site de atendimento espiritual com tradição afro-brasileira.",
     image: "/projects/ninadeoxum.png",
-    url: "https://ninadeoxum.netlify.app",
+    url: "https://ninadeoxum.com",
+  },
+  {
+    title: "Box Zero 3",
+    description:
+      "Desenvolvi esse site para a oficina mecânica Box Zero 3, premiada há 8 anos como a melhor de Arroio do Sal - RS.",
+    image: "/projects/boxzero3.png",
+    url: "https://boxzero3.netlify.app",
   },
 ];
